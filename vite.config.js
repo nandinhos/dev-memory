4fdb13daf5d02cb7808a286a1952613d83bbe2af
Nando Dev <nandinhos@gmail.com>
2026-07-31 00:19:17 -0300

    fix (vite): configura hmr e origin no vite.config.js para suporte ao ambiente docker dev

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,13 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        origin: 'http://localhost:25173',
+        hmr: {
+            host: 'localhost',
+            port: 25173,
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
